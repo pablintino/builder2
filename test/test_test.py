b70c4190ab4d482b317be8b825d0222a012eca99
Pablo Rodriguez Nava <pablintino@hotmail.com>
2022-07-31 21:21:01 +0200

    Created first tool basic test (for JDK installer) and formated test sources with black style

--- test/test_test.py
+++ test/test_test.py
@@ -2,6 +2,5 @@ from compilers_support import get_gcc_wellknown_paths
 
 
 def test_test():
-    paths = get_gcc_wellknown_paths('/opt/compilers/gcc-10.3.0')
+    paths = get_gcc_wellknown_paths("/opt/compilers/gcc-10.3.0")
     print(paths)
-
--- test/tool_installers_test.py
+++ test/tool_installers_test.py
@@ -2,106 +2,135 @@ import os
 from unittest.mock import patch, MagicMock
 from urllib.parse import urlparse
 
-import java_support
-from execution_parameters import ExecutionParameters
 from models.metadata_models import JdkConfiguration
-from tool_installers import DownloadOnlySourcesInstaller, JdkInstaller
+from tools import java_support
+from tools.tool_installers import JdkInstaller
 
 
-@patch('builder2.tool_installers.file_utils.compute_file_sha1')
-@patch('builder2.tool_installers.file_utils.download_file')
-@patch('builder2.tool_installers.file_utils.extract_file')
-@patch('builder2.tool_installers.pathlib.Path')
-@patch('builder2.tool_installers.shutil.copytree')
-@patch('builder2.tool_installers.tempfile.TemporaryDirectory')
-def test_basic_all_ok(tempfile_mock, copytree_mock, pathlib_path_mock, extract_file_mock,
-                      download_file_mock, compute_file_sha1_mock):
-    config = {
-        "group": "test-group",
-        "type": "download-only-compiler",
-        "name": "test-tool",
-        "version": "1.0.0",
-        "url": "https://test.test.com/test-file-v.0.0.tar.bz2"
-    }
-
-    # Make the temp file return a static path always
-    tempfile_mock.return_value = tempfile_mock
-    cleanup_mock = MagicMock()
-    tempfile_mock.configure_mock(name='/test', cleanup=cleanup_mock)
-
-    tool_extract_path = os.path.join(tempfile_mock.name, 'test-tool')
-    extract_file_mock.return_value = tool_extract_path
-
-    parameters = ExecutionParameters(core_count=1, file_name='/file-name', time_multiplier=1.0,
-                                     target_dir='/target-dir')
-    with DownloadOnlySourcesInstaller('test-tool', config, parameters) as installer:
-        result = installer.run_installation()
-        assert result.success
-        assert not result.error
-
-    tarfile_target_path = os.path.join(
-        tempfile_mock.name, os.path.basename(urlparse(config['url']).path)
-    )
-    download_file_mock.assert_called_once_with(config['url'], tarfile_target_path)
-    compute_file_sha1_mock.assert_called_once_with(tarfile_target_path)
-    extract_file_mock.assert_called_once_with(tarfile_target_path, tempfile_mock.name)
-    pathlib_path_mock.assert_not_called()
-    copytree_mock.assert_called_once_with(tool_extract_path, os.path.join('/test', config["group"], 'test-tool'))
-    cleanup_mock.assert_called_once()
-
-
-@patch('builder2.tool_installers.java_support.get_jdk_wellknown_paths')
-@patch('builder2.tool_installers.crypto_utils.compute_file_sha1')
-@patch('builder2.tool_installers.file_utils.download_file')
-@patch('builder2.tool_installers.file_utils.extract_file')
-@patch('builder2.tool_installers.shutil.copytree')
-@patch('builder2.tool_installers.tempfile.TemporaryDirectory')
-@patch('builder2.tool_installers.file_utils.read_file_as_text')
-def test_jdk_basic(read_file_as_text_mock, tempfile_mock, copytree_mock, pathlib_path_mock,
-                   extract_file_mock, download_file_mock, compute_file_sha1_mock, get_jdk_wellknown_paths_mock):
+@patch("builder2.tools.tool_installers.tempfile.TemporaryDirectory")
+@patch("builder2.tools.tool_installers.pathlib.Path")
+def test_jdk_basic(pathlib_path_mock, tempfile_mock):
     config = JdkConfiguration(
-        name='test-tool',
-        url='https://test.test.com/test-file-v.0.0.tar.bz2',
-        default=False,
-        add_to_path=False,
-        expected_hash=None,
-        group='test-group',
-        version='1.0.0',
-        required_packages=[]
+        name="test-tool",
+        url="https://test.test.com/test-file-v.0.0.tar.bz2",
+        default=True,
+        add_to_path=True,
+        expected_hash="04582c237a0516d67f6ccec3c32e3e40e114c219",
+        group="test-group",
+        required_packages=["dummy-package"],
     )
+
+    installation_base_dir = "/base/dir"
+    tool_key = "test-tool"
+    target_dir = os.path.join(installation_base_dir, config.group, tool_key)
 
     # Make the temp file return a static path always
     tempfile_mock.return_value = tempfile_mock
     cleanup_mock = MagicMock()
-    tempfile_mock.configure_mock(name='/test', cleanup=cleanup_mock)
+    temp_path = "/test-temp"
+    tempfile_mock.configure_mock(name=temp_path, cleanup=cleanup_mock)
 
-    tool_extract_path = os.path.join(tempfile_mock.name, 'test-tool')
-    extract_file_mock.return_value = tool_extract_path
-    compute_file_sha1_mock.return_value = '98ab7aefeaa9ee253eaa30c50d3e6c9eb8d863bf'
-
-    get_jdk_wellknown_paths_mock.return_value = {
-        java_support.EXEC_NAME_JAVA: '/test/java',
-        java_support.DIR_NAME_JAVA_HOME: '/test',
-        java_support.EXEC_NAME_JAVA_CACERTS: '/test/cacerts',
-        java_support.EXEC_NAME_JAVA_DOC: '/test/javadoc',
-        java_support.EXEC_NAME_JAVA_KEYTOOL: '/test/keytool'
+    java_tools_mock = MagicMock()
+    java_tools_mock.get_jdk_wellknown_paths.return_value = {
+        java_support.EXEC_NAME_JAVA: "/test/java",
+        java_support.DIR_NAME_JAVA_HOME: "/test",
+        java_support.EXEC_NAME_JAVA_CACERTS: "/test/cacerts",
+        java_support.EXEC_NAME_JAVA_DOC: "/test/javadoc",
+        java_support.EXEC_NAME_JAVA_KEYTOOL: "/test/keytool",
     }
 
-    with open(os.path.join(os.path.join(os.path.dirname(__file__), 'data', 'jdk-release-file')), 'r') as f:
-        read_file_as_text_mock.return_value = f.read()
+    file_manager = MagicMock()
+    cryptographic_provider = MagicMock()
+    command_runner_mock = MagicMock()
+    package_manager_mock = MagicMock()
 
-    parameters = ExecutionParameters(core_count=1, file_name='/file-name', time_multiplier=1.0,
-                                     target_dir='/target-dir')
-    with JdkInstaller('test-tool', config, parameters) as installer:
-        installation = installer.run_installation()
-        assert installation.wellknown_paths == get_jdk_wellknown_paths_mock.return_value
-        assert installation.package_hash == compute_file_sha1_mock.return_value
+    # Fake pathlib exists to test /bin existence
+    target_dir_base_path_mock = MagicMock()
 
-    tarfile_target_path = os.path.join(
-        tempfile_mock.name, os.path.basename(urlparse(config.url).path)
+    def pathlib_side_effect(*args, **kwargs):
+        if args[0] == target_dir:
+            return target_dir_base_path_mock
+        return None
+
+    pathlib_path_mock.side_effect = pathlib_side_effect
+    bin_path_mock = MagicMock()
+    target_dir_base_path_mock.joinpath.return_value = bin_path_mock
+    bin_path_mock.exists.return_value = True
+    bin_path_mock.is_dir.return_value = True
+    bin_path_mock.absolute.return_value = os.path.join(target_dir, "bin")
+
+    # Fake tar file destination path
+    tar_file_path = os.path.join(temp_path, os.path.basename(urlparse(config.url).path))
+
+    # Fake package computed hash
+    cryptographic_provider.compute_file_sha1.return_value = (
+        "04582c237a0516d0af6bbec3cace3e40e113e219"
     )
-    download_file_mock.assert_called_once_with(config.url, tarfile_target_path)
-    compute_file_sha1_mock.assert_called_once_with(tarfile_target_path)
-    extract_file_mock.assert_called_once_with(tarfile_target_path, tempfile_mock.name)
-    copytree_mock.assert_called_once_with(tool_extract_path, os.path.join('/test', config.group, 'test-tool'))
+
+    # Tar file extract directory
+    sources_directory = os.path.join(temp_path, "test-file-v.0.0")
+    file_manager.extract_file.return_value = sources_directory
+
+    # Mock release file version search
+    file_manager.read_file_and_search_group.return_value = "v9.9.9"
+
+    with JdkInstaller(
+        tool_key,
+        config,
+        installation_base_dir,
+        file_manager=file_manager,
+        cryptographic_provider=cryptographic_provider,
+        command_runner=command_runner_mock,
+        package_manager=package_manager_mock,
+        java_tools=java_tools_mock,
+    ) as installer:
+        installation = installer.run_installation()
+        assert installation
+        assert installation.name == config.name
+        assert installation.path == target_dir
+        assert (
+            installation.package_hash
+            == cryptographic_provider.compute_file_sha1.return_value
+        )
+        assert (
+            installation.version == file_manager.read_file_and_search_group.return_value
+        )
+        assert (
+            installation.wellknown_paths
+            == java_tools_mock.get_jdk_wellknown_paths.return_value
+        )
+        assert "JAVA_HOME" in installation.environment_vars
+        assert (
+            installation.environment_vars["JAVA_HOME"]
+            == java_tools_mock.get_jdk_wellknown_paths.return_value[
+                java_support.DIR_NAME_JAVA_HOME
+            ]
+        )
+        assert installation.path_dirs and installation.path_dirs[0] == str(
+            os.path.join(target_dir, "bin")
+        )
+
+    # Ensure tool is copied to its final destination
+    file_manager.copy_file_tree.assert_called_once_with(sources_directory, target_dir)
+
+    # Ensure tar file is extracted from sources dir to the temp dir
+    file_manager.extract_file.assert_called_once_with(tar_file_path, temp_path)
+
+    # Ensure hash is computed for the tarfile
+    cryptographic_provider.compute_file_sha1.assert_called_once_with(tar_file_path)
+
+    # Ensure hash is validated
+    cryptographic_provider.validate_file_hash.assert_called_once_with(
+        tar_file_path, config.expected_hash
+    )
+
+    # Ensure required packages are installed
+    package_manager_mock.install_packages.assert_called_once_with(
+        config.required_packages
+    )
+
+    # Ensure /bin path has been used for search for path dirs
+    target_dir_base_path_mock.joinpath.assert_called_once_with("bin")
+
+    file_manager.create_file_tree.assert_not_called()
     cleanup_mock.assert_called_once()
