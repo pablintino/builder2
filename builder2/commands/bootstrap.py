567db155be3674ea1b5900038c783e70c40feae2
Pablo Rodriguez Nava <pablintino@hotmail.com>
2025-07-13 14:56:06 +0200

    Add user swapping in bootstrap command

--- builder2/commands/bootstrap.py
+++ builder2/commands/bootstrap.py
@@ -20,6 +20,19 @@ __logger = logging.getLogger(__name__)
 __FALLBACK_SHELLS = ["/bin/bash, /bin/sh"]
 
 
+def __fetch_user_group(user_id: str):
+    try:
+        user_id_str, group_id_str = user_id.split(":", 1)
+        user_id = int(user_id_str)
+        group_id = int(group_id_str)
+        return user_id, group_id
+    except ValueError as e:
+        __logger.error(
+            "Cannot parse the given user and group IDs %s. Exiting.", user_id
+        )
+        sys.exit(2)
+
+
 def __get_fallback_shell():
     for shell in __FALLBACK_SHELLS:
         try:
@@ -70,6 +83,51 @@ def __signal_handler(_, __):
     sys.exit(130)
 
 
+def __user_exists(user_id: int) -> bool:
+    try:
+        pwd.getpwuid(user_id)
+        return True
+    except KeyError:
+        return False
+
+
+def __create_user(uid):
+    if __user_exists(uid):
+        return
+    try:
+        # copy the shell from the running user
+        default_shell = __get_default_shell()
+        subprocess.check_call(
+            ["useradd", "-u", str(uid), "devuser", "-ms", default_shell],
+            text=True,
+        )
+    except subprocess.CalledProcessError:
+        __logger.error("Cannot parse create a devuser with IDs %d. Exiting.", uid)
+        sys.exit(3)
+
+
+def __setup_user(user_id: str) -> None:
+    try:
+        if not user_id:
+            return
+
+        uid, gid = __fetch_user_group(user_id)
+
+        # if we are already using the requested user just do nothing
+        if uid == os.getuid():
+            return
+
+        __create_user(uid)
+
+        os.setgid(gid)
+        os.setuid(uid)
+
+        os.environ["HOME"] = pwd.getpwuid(uid).pw_dir  # user must exist, it can't fail
+    except OSError:
+        __logger.error("Cannot switch to the user ID %s. Exiting.", user_id)
+        sys.exit(4)
+
+
 def __bootstrap(
     args,
 ):
@@ -92,6 +150,7 @@ def __bootstrap(
                 "Skipping loading certificates. %s does not exist", args.certs_dir
             )
 
+        __setup_user(args.user_id)
         bootstrap_cmd = __prepare_command(args)
         env_vars = builder2.environment_builder.build_environment_variables(
             installation_summary, args.generate_vars
@@ -120,5 +179,11 @@ def register(subparsers):
         action="store_true",
         env_var=constants.ENV_VAR_GENERATE_VARS,
         help="Enable component generated environment variables",
+    )
+    command_parser.add_argument(
+        "--user",
+        dest="user_id",
+        env_var=constants.ENV_VAR_BOOTSTRAP_UID,
+        help="The user and group IDs in format <user>:<group> to switch to",
     )
     command_parser.add_argument("remainder", nargs=configargparse.REMAINDER)
